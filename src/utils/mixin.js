1e691923e52f8c1ce87c6aa20a3bb8d5921e5e0e
Tik Guan <275151754@qq.com>
2020-04-15 10:13:53 +0800

    优化errHandler错误处理

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -95,9 +95,27 @@ const mixin = {
           Toast(e);
 
         } else {
-          Toast('未知错误');
+          const errTips = String(e);
+          const code400 = 'code 400';  // 请求语法错误
+          const code404 = 'code 404';  // 地址错误
+          const code405 = 'code 405';  // 请求方法错误
+          const code500 = 'code 500';  // 服务端错误
+          const timeout = 'timeout';
+          if (errTips.indexOf(code400) === -1) {
+            Toast('错误码: 400, 请求语法错误.');
+          } else if (errTips.indexOf(code404) === -1) {
+            Toast('错误码: 404, 请求地址错误.');
+          } else if (errTips.indexOf(code405) === -1) {
+            Toast('错误码: 405, 请求方法错误.');
+          } else if (errTips.indexOf(code500) === -1) {
+            Toast('系统开小差了~');
+          } else if (errTips.indexOf(timeout) === -1) {
+            Toast('请求超时.');
+          } else {
+            Toast('未知错误.');
+          };
 
-        }
+        };
       }
     }
 
