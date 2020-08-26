a3363323c19e1fdf6c00acf6dffc9ba328376edb
Tik Guan <275151754@qq.com>
2020-08-26 11:51:15 +0800

    优化HttpConfige

--- src/api/HttpConfige.js
+++ src/api/HttpConfige.js
@@ -1,26 +1,14 @@
 let SERVER = {
-  WECHAT: '',
-  ALOHA: ''
+  WECHAT: 'https://wechat-qa.walmartmobile.cn',
+  ALOHA: 'https://aloha-qa.walmartmobile.cn'
 };
 
 const ENV = process.env.NODE_ENV;
 const HOST = process.env.VUE_APP_HOST;
 
-const QA_WECHAT = 'https://wechat-qa.walmartmobile.cn';
-const QA_ALOHA = 'https://aloha-qa.walmartmobile.cn';
-
-const PROD_WECHAT = 'https://wechat.walmartmobile.cn';
-const PROD_ALOHA = 'https://aloha.walmartmobile.cn';
-
-if (ENV === 'development' || (ENV === 'production' && HOST === 'qa')) {
-  
-  SERVER.WECHAT = QA_WECHAT;
-  SERVER.ALOHA = QA_ALOHA;
-
-} else if (ENV === 'production' && HOST === 'prod') {
-
-  SERVER.WECHAT = PROD_WECHAT;
-  SERVER.ALOHA = PROD_ALOHA;
-
+if (ENV === 'production' && HOST === 'prod') {
+  SERVER.WECHAT = 'https://wechat.walmartmobile.cn';
+  SERVER.ALOHA = 'https://aloha.walmartmobile.cn';
 }
+
 export default SERVER;
