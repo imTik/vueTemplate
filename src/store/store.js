4ac78e0a2064f8ff691b607c6a3ccd04b737bc5b
Tik Guan <275151754@qq.com>
2020-10-28 09:17:20 +0800

    完善组件缓存判断

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 import LOCAL from '../utils/localStorageFn';
 // import packageConfig from '../../package';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     // appName: packageConfig.name,
@@ -12,51 +12,44 @@ export default new Vuex.Store({
     keepAliveLists: [],
     userInfo: null,
 
-    token: '',
-
+    token: ''
   },
   mutations: {
-
     // 保存用户信息
-    SAVE_USER_INFO (state, data) {
+    SAVE_USER_INFO(state, data) {
       state.userInfo = data;
       LOCAL.set('USER_INFO', data);
     },
 
-    SAVE_TOKEN (state, data) {
+    SAVE_TOKEN(state, data) {
       state.token = data;
       LOCAL.set('TOKEN', data);
     },
 
     // 保存组件缓存列表
-    SAVE_KEEP_ALIVE (state, data) {
-      if (!data.status) return;
-      let index = state.keepAliveLists.indexOf(data.name);
-      if (data.status === 'save') {
-        if (index === -1) state.keepAliveLists.push(data.name);
-      } else if (data.status === 'del') {
-        let index = state.keepAliveLists.indexOf(data.name);
-        state.keepAliveLists.splice(index, 1);
-      }
+    SAVE_KEEP_ALIVE(state, name) {
+      let index = state.keepAliveLists.indexOf(name);
+      if (index === -1) state.keepAliveLists.push(name);
+    },
+    DEL_KEEP_ALIVE(state, name) {
+      let index = state.keepAliveLists.indexOf(name);
+      if (index > -1) state.keepAliveLists.splice(index, 1);
     }
-
   },
   getters: {
-
     // APP_NAME: state => state.appName,
     INSIDE_APP_NAME: state => state.insideAppName,
-    
+
     KEEP_ALIVE_LISTS: state => state.keepAliveLists,
 
     USER_INFO: state => {
-      if(!state.userInfo) state.userInfo = LOCAL.get('USER_INFO');
+      if (!state.userInfo) state.userInfo = LOCAL.get('USER_INFO');
       return state.userInfo;
     },
 
     TOKEN: state => {
-      if(!state.token) state.token = LOCAL.get('TOKEN');
+      if (!state.token) state.token = LOCAL.get('TOKEN');
       return state.token;
-    },
-
-  },
-})
+    }
+  }
+});
