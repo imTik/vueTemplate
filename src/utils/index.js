e3957f256d81ab8da1fd526f383e4a4b2bd142a8
Tik Guan <275151754@qq.com>
2020-12-07 16:07:27 +0800

    优化日期方法，兼容ios端

--- src/utils/index.js
+++ src/utils/index.js
@@ -50,11 +50,34 @@ export const REX_MAP = {
   }
 };
 
+const BROWSER_INFO = getBrowseInfo();
+export function getBrowseInfo() {
+  let u = navigator.userAgent;
+  return {
+    trident: u.indexOf('Trident') > -1, //IE内核
+    presto: u.indexOf('Presto') > -1, //opera内核
+    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+    android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+    iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+    iPad: u.indexOf('iPad') > -1, //是否iPad
+    webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+    weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+    qq: u.match(/\sQQ/i) == ' qq', //是否QQ
+    language: (navigator.browserLanguage || navigator.language).toLowerCase()
+  };
+}
+
 /**
  * @param {String} type FULL(完整日期)/YMD(日期))/YM(年月)/T(时间)/TAMP(时间戳)
  */
 export function getNowDate(type = 'FULL', tamp) {
+  // console.log('拿到的日期', tamp);
   if (tamp && typeof tamp === 'string') tamp = tamp.split('.')[0]; // 防止 2020-11-30T01:46:44.490+0000 格式的日期
+  if (BROWSER_INFO.ios && typeof tamp === 'string' && tamp.indexOf('-')) tamp = iosDate(tamp);
+  // console.log('处理后的日期', tamp);
   const _date = tamp ? new Date(tamp) : new Date();
   let Y = _date.getFullYear();
   let M = _date.getMonth() + 1;
@@ -77,7 +100,11 @@ export function getNowDate(type = 'FULL', tamp) {
     T: H + ':' + Mi + ':' + S
   };
   type = type.toUpperCase();
-  return MODE[type] ? MODE[type] : null;
+
+  let OUTPUT = MODE[type];
+  if (BROWSER_INFO.ios && typeof OUTPUT === 'string' && OUTPUT.indexOf('-')) OUTPUT = iosDate(OUTPUT);
+  // console.log('输出的日期', OUTPUT);
+  return OUTPUT;
 }
 
 // 获取指定日期
